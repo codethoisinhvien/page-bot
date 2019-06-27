16fc551dbe803f807e8bfd27b8629152cf994d11
Nguyen Truong Giang <nguyentruonggiangptdq@gmail.com>
2019-06-27 17:08:52 +0700

    test

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 @app.route('/hello')
 def hello():
     return "hello"
+
 @app.route('/api/test')
 def send():
     return jsonify({"user":"pt","message":"đã xem"})
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print(data)
+    return "ok", 200
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
